98a480c0c9ac1e54e376a335040578d6dcf6f87a
Zartoru <bassez.claude97@gmail.com>
2020-11-23 17:33:44 +0100

    ajout d'un fichier tag

---
+++ tag.php
@@ -0,0 +1,15 @@
+<?php
+
+//code php
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa labore, inventore error accusantium, aperiam quisquam beatae nostrum expedita soluta, optio delectus eum odit saepe cumque nam quae quos nobis in!</p>
+</body>
+</html>
